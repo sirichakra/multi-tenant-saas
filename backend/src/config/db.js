9cfcada3f737719102406483eca16dc3be45d083
sirichakra <147473243+sirichakra@users.noreply.github.com>
2026-01-03 10:46:46 +0530

    Commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,12 +5,12 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+export const pool = new Pool({
+  host: process.env.DB_HOST || "database",
+  port: 5432,
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_NAME || "saas_db",
 });
 
 export const connectDB = async () => {
